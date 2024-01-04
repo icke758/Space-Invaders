f5ad175f603dc99d56daf032e9648d9f7005dbdb
Gabriel L. <gabriellovato15@gmail.com>
2024-01-04 15:59:03 -0300

    Adjusted Bullet collision layers

--- entities/bullet/Bullet.cs
+++ entities/bullet/Bullet.cs
@@ -4,7 +4,13 @@ public partial class Bullet : RigidBody2D
 {
 	public override void _Ready()
 	{
+		this.BodyEntered += (body) => HandleCollision(body);
+
 		Timer timer = GetNode<Timer>("Timer");
 		timer.Timeout += () => QueueFree();
+	}
+
+	private void HandleCollision(Node body) {
+		//TODO: Actually handle collision
 	}
 }
