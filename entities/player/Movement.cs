bd65d33a2e419b4fb082f4d9c868e4f862a28d6a
Gabriel L. <gabriellovato15@gmail.com>
2024-01-04 15:59:54 -0300

    Player scene and new movement with acceleration

--- entities/player/Movement.cs
+++ entities/player/Movement.cs
@@ -1,29 +1,71 @@
 using Godot;
-using System;
 
 public partial class Movement : CharacterBody2D
 {
-	private readonly float MOVE_SPEED = 150.0f;
+	[Export] float speed = 150.0f;
+	[Export] private float ACCELERATION = 200.0f;
+	[Export] private float DEACCELERATION = 200.0f;
+	[Export] private float ROTATION_VELOCITY = 15.0f;
 
-	public override void _PhysicsProcess(double delta)
+	[Export] private Node2D _ship;
+
+	private Vector2 _inputVelocity;
+
+	// Action names
+	private const string LEFT 	= "pl_left";
+	private const string RIGHT 	= "pl_right";
+	private const string UP 	= "pl_up";
+	private const string DOWN 	= "pl_down";
+
+    public override void _Ready()
+    {
+		_ship ??= GetNode<Node2D>("Ship");
+    }
+
+    public override void _Process(double delta)
+    {
+		_inputVelocity = new Vector2(
+			Input.GetAxis(LEFT, RIGHT),
+			Input.GetAxis(UP, DOWN)
+		).Normalized();
+
+		if (_inputVelocity != Vector2.Zero)
+		{
+			float speeder = 1.0f;
+
+			// This makes the ship turn almost instantly if we try
+			// turning it a full 180 degrees.
+			// The dot product of two vectors tells us how similar they are
+			// If they are pointing to the same direction, 1.0 is returned
+			// If they are perpendicular, 0.0 is returned
+			// -1.0 is returned if they are facing opposite directions
+			// This function interpolates its result, so any negative value
+			// mean opposing vectors
+			// We that and if true, we increase the acceleration momentarily 
+			if(_inputVelocity.Dot(Velocity.Normalized()) < 0) {
+				speeder = 20.0f;
+			}
+
+			Velocity = Velocity.MoveToward(_inputVelocity * speed, ACCELERATION * speeder * (float) delta);
+			UpdateShipRotation(delta);
+		} 
+		else 
+		{
+			Velocity = Velocity.MoveToward(Vector2.Zero, DEACCELERATION * (float) delta);
+		}
+
+    }
+
+	private void UpdateShipRotation(double delta)
 	{
-		Vector2 velocity = Velocity;
+		float oldRotation = _ship.Rotation;
+		float newRotation = Velocity.Angle();
 
-		velocity.X = 0;
-		velocity.Y = 0;
-		if(Input.IsKeyPressed(Key.A))
-			velocity.X = - MOVE_SPEED;
+		_ship.Rotation = (float) Mathf.LerpAngle(oldRotation, newRotation, delta * ROTATION_VELOCITY);
+	}
 
-		else if(Input.IsKeyPressed(Key.D))
-			velocity.X = MOVE_SPEED;
-
-		else if(Input.IsKeyPressed(Key.W))
-			velocity.Y = - MOVE_SPEED;
-			
-		else if(Input.IsKeyPressed(Key.S))
-			velocity.Y = MOVE_SPEED;
-		
-		Velocity = velocity;
+    public override void _PhysicsProcess(double delta)
+	{
 		MoveAndSlide(); 
 	}
 }
