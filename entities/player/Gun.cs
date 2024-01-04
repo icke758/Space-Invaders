d8e38e7d7013bdfc9129922adadc201da6209a2c
Gabriel L. <gabriellovato15@gmail.com>
2024-01-04 16:00:42 -0300

    Gun code refactor, spawning at muzzle

--- entities/player/Gun.cs
+++ entities/player/Gun.cs
@@ -3,32 +3,52 @@ using Godot;
 public partial class Gun : Node2D
 {
 	[Export] PackedScene bullet_tscn;
+	[Export] Node2D muzzle;
+
 	[Export] float BULLET_SPEED = 600f;
-	[Export] float BULLET_PER_SECOND = 5f;
+	[Export] float BULLET_PER_SECOND = 15f;
 	[Export] float BULLET_DAMAGE = 30f;
 
-	private float fireRate;
+	private float _cooldownTime;
+	private float _fireCountdown = 0f;
 
-	private float TIME_UNTIL_FIRE = 0f;
+	// Action names
+	private const string FIRE = "pl_fire";
 
 	public override void _Ready()
 	{
-		fireRate = 1 / BULLET_PER_SECOND;
+		muzzle ??= GetNode<Node2D>("Muzzle");
+		_cooldownTime = 1 / BULLET_PER_SECOND;
 	}
 
 	public override void _Process(double delta)
 	{
-		if(Input.IsActionJustPressed("click") && TIME_UNTIL_FIRE > fireRate) {
-			RigidBody2D bullet = bullet_tscn.Instantiate<RigidBody2D>();
-			bullet.Rotation = GlobalRotation;
-			bullet.GlobalPosition = GlobalPosition;
-			bullet.LinearVelocity = bullet.Transform.X * BULLET_SPEED;
+		if(Input.IsActionPressed(FIRE) && _fireCountdown <= 0)
+		{
+			_fireCountdown = _cooldownTime;
 
-			GetTree().Root.AddChild(bullet);
-
-			TIME_UNTIL_FIRE = 0f;
-		} else {
-			TIME_UNTIL_FIRE += (float)delta;
+			Fire();
+		} 
+		else 
+		{
+			_fireCountdown -= (float) delta;
 		}
+
+		RotateToMouse();
+	}
+
+	private void RotateToMouse() {
+		Rotate(GetAngleTo(GetGlobalMousePosition()));
+	}
+
+	private void Fire() {
+		RigidBody2D bullet = bullet_tscn.Instantiate<RigidBody2D>();
+
+		bullet.ZIndex = -1;
+		bullet.Rotation = GlobalRotation;
+		bullet.GlobalPosition = muzzle.GlobalPosition;
+		bullet.LinearVelocity = bullet.Transform.X * BULLET_SPEED;
+
+		GetTree().Root.AddChild(bullet);
 	}
 }
